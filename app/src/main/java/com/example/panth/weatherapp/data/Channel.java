289b15f943137248b07f74df87b25219051d78b3
carlso70 <carlso70@purdue.edu>
2016-07-14 21:35:53 -0500

    Implemented Channel and Condition data models for the JSON from yahoo

--- app/src/main/java/com/example/panth/weatherapp/data/Channel.java
+++ app/src/main/java/com/example/panth/weatherapp/data/Channel.java
@@ -6,8 +6,22 @@ import org.json.JSONObject;
  * Created by panth on 7/14/2016.
  */
 public class Channel implements JSONPopulator {
+    private Units units;
+    private Item item;
     @Override
     public void populate(JSONObject data) {
+        units = new Units();
+        units.populate(data.optJSONObject("units"));
 
+        item = new Item();
+        item.populate(data.optJSONObject("item"));
+    }
+
+    public Units getUnits() {
+        return units;
+    }
+
+    public Item getItem(){
+        return item;
     }
 }
--- app/src/main/java/com/example/panth/weatherapp/data/Condition.java
+++ app/src/main/java/com/example/panth/weatherapp/data/Condition.java
@@ -5,9 +5,29 @@ import org.json.JSONObject;
 /**
  * Created by panth on 7/14/2016.
  */
-public class Condition implements JSONPopulator{
+public class Condition implements JSONPopulator {
+
+    private int code;
+    private int temperature;
+    private String description;
+
     @Override
     public void populate(JSONObject data) {
-
+        code = data.optInt("code");
+        temperature = data.optInt("temp");
+        description = data.optString("text");
     }
+
+    public int getCode() {
+        return code;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
